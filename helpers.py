0e43553d14b874aee51a5007f5613478b88d5857
szymci0 <szymonbetk@gmail.com>
2023-10-18 15:11:45 +0200

    Add unit test for ngrams function

--- helpers.py
+++ helpers.py
@@ -1,10 +1,10 @@
 from itertools import tee
 
 
-def ngrams(sequence: [], n: int):
+def ngrams(sequence, n: int):
     """
     Simplified https://tedboy.github.io/nlps/generated/generated/nltk.ngrams.html.
-    Return n-length ngrams generated from sequence in the array.
+    Return n-length ngrams generated from sequence in the iterable.
 
     ngrams(range(6), 2) --> [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]
     ngrams(range(6), 3) --> [(0, 1, 2), (1, 2, 3), (2, 3, 4), (3, 4, 5)]
--- unit_tests.py
+++ unit_tests.py
@@ -3,6 +3,7 @@ from stats import (
     count_letters,
     keywords_extraction,
 )
+from helpers import ngrams
 
 TEST_TEXT1 = """
     Paste your text here or start fresh. Duplicates are highlighted in real-time. (Once you begin, these instructions will be cleared.)
@@ -38,6 +39,15 @@ def count_words_test():
     assert result == 47
 
 
+def ngrams_test():
+    result = ngrams(range(8), 3)
+    assert result == [(0, 1, 2), (1, 2, 3), (2, 3, 4), (3, 4, 5), (4, 5, 6), (5, 6, 7)]
+    result = ngrams(range(6), 2)
+    assert result == [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]
+    result = ngrams(range(1), 2)
+    assert result == []
+
+
 def keywords_extraction_test():
     result = keywords_extraction(TEST_TEXT1, num_of_keys=5)
     assert result == ['text', 'duplicates', 'next', 'use', 'buttons']
@@ -50,5 +60,6 @@ def keywords_extraction_test():
 def perform_tests():
     count_letters_test()
     count_words_test()
+    ngrams_test()
     keywords_extraction_test()
     print("All test passed!")
